aa299f544200cd80dea9ff3e2c7a6c5e9589f0ec
JinWei0811 <31277660+JinWei0811@users.noreply.github.com>
2021-02-05 12:35:26 +0800

    移除Console.log

--- 熊班長實況助手/betting.js
+++ 熊班長實況助手/betting.js
@@ -7,13 +7,13 @@ function detectBet() {
         var activityTabbet = document.getElementsByClassName("tw-align-items-center tw-align-middle tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-border-top-left-radius-medium tw-border-top-right-radius-medium tw-core-button tw-core-button--text tw-inline-flex tw-justify-content-center tw-overflow-hidden tw-relative")[0];
         // activityTabbet.click();
         setTimeout(activityTabbet.click(), 500);
-        console.log('click1');
+        // console.log('click1');
         var bettest = document.getElementsByClassName("predictions-list-item__body tw-align-items-center tw-border-radius-medium tw-c-background-alt tw-flex tw-full-width")[0];
         // bettest.click();
         setTimeout(bettest.click(), 500);
-        console.log('click2');
+        // console.log('click2');
         var bettest0 = document.getElementsByClassName("fixed-prediction-button fixed-prediction-button--blue tw-align-items-center tw-border-radius-small tw-flex tw-pd-x-1 tw-pd-y-05")[0];
-        console.log(currentDateTime, '  bettest0  ', bettest0);
+        // console.log(currentDateTime, '  bettest0  ', bettest0);
         if (bettest0 !== undefined) {
             chrome.runtime.sendMessage({ content: true });
         }
@@ -21,7 +21,7 @@ function detectBet() {
         // var close = document.getElementsByClassName("tw-align-items-center tw-align-middle tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-border-top-left-radius-medium tw-border-top-right-radius-medium tw-button-icon tw-button-icon--secondary tw-core-button tw-inline-flex tw-justify-content-center tw-overflow-hidden tw-relative");
         // close.click();
         setTimeout(activityTabbet.click(), 1200);
-        console.log('click3 ', close);
+        // console.log('click3 ', close);
 
         // console.log(currentDateTime + ' detectbet');
     } catch (error) {
