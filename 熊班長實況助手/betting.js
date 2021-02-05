11ea3a2ea8dd0f271c467b19f66cfca33b03bf47
JinWei0811 <31277660+JinWei0811@users.noreply.github.com>
2021-02-05 12:19:20 +0800

    新增 Click Delay Time

--- 熊班長實況助手/betting.js
+++ 熊班長實況助手/betting.js
@@ -5,16 +5,22 @@ function detectBet() {
         var nowt = today.getSeconds() < 10 ? "0" + today.getSeconds() : today.getSeconds();
         var currentDateTime = today.getHours() + ':' + nowm + ":" + nowt;
         var activityTabbet = document.getElementsByClassName("tw-align-items-center tw-align-middle tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-border-top-left-radius-medium tw-border-top-right-radius-medium tw-core-button tw-core-button--text tw-inline-flex tw-justify-content-center tw-overflow-hidden tw-relative")[0];
-        activityTabbet.click();
+        // activityTabbet.click();
+        setTimeout(activityTabbet.click(), 100);
+
         var bettest = document.getElementsByClassName("predictions-list-item__body tw-align-items-center tw-border-radius-medium tw-c-background-alt tw-flex tw-full-width")[0];
-        bettest.click();
+        // bettest.click();
+        setTimeout(bettest.click(), 100);
         var bettest0 = document.getElementsByClassName("fixed-prediction-button fixed-prediction-button--blue tw-align-items-center tw-border-radius-small tw-flex tw-pd-x-1 tw-pd-y-05")[0];
         console.log(currentDateTime, '  bettest0  ', bettest0);
         if (bettest0 !== undefined) {
             chrome.runtime.sendMessage({ content: true });
         }
         var close = document.getElementsByClassName("tw-align-items-center tw-align-middle tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-border-top-left-radius-medium tw-border-top-right-radius-medium tw-button-icon tw-button-icon--secondary tw-core-button tw-inline-flex tw-justify-content-center tw-overflow-hidden tw-relative");
-        close.click();
+        var close = document.getElementsByClassName("tw-align-items-center tw-align-middle tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-border-top-left-radius-medium tw-border-top-right-radius-medium tw-button-icon tw-button-icon--secondary tw-core-button tw-inline-flex tw-justify-content-center tw-overflow-hidden tw-relative");
+        // close.click();
+        setTimeout(close.click(), 100);
+
         // console.log(currentDateTime + ' detectbet');
     } catch (error) {}
 
