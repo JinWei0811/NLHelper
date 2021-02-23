43b2d6e4b6c5a99ebb02589fc7ecca05ea639c5b
JinWei0811 <31277660+JinWei0811@users.noreply.github.com>
2021-02-23 11:44:58 +0800

    第N刺修正賭盤

--- 熊班長實況助手/betting.js
+++ 熊班長實況助手/betting.js
@@ -1,34 +1,41 @@
 function detectBet() {
     try {
-        var today = new Date();
-        var nowm = today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
-        var nowt = today.getSeconds() < 10 ? "0" + today.getSeconds() : today.getSeconds();
-        var currentDateTime = today.getHours() + ':' + nowm + ":" + nowt;
+        // var today = new Date();
+        // var nowm = today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
+        // var nowt = today.getSeconds() < 10 ? "0" + today.getSeconds() : today.getSeconds();
+        // var currentDateTime = today.getHours() + ':' + nowm + ":" + nowt;
         // console.log(currentDateTime + '  Bet Detect');
+        // var activityFirst = document.getElementsByClassName("ScCoreButton-sc-1qn4ixc-0 ScCoreButtonText-sc-1qn4ixc-3 kGWcAX tw-core-button")[0];
+        activityF(); // 打開第一層
 
-        // 開啟第一層
-        var activityTabbet = document.getElementsByClassName("ScCoreButton-sc-1qn4ixc-0 ScCoreButtonText-sc-1qn4ixc-3 kGWcAX tw-core-button")[0];
-        // 開啟第二層
-        var bettest = document.getElementsByClassName("predictions-list-item__body tw-align-items-center tw-border-radius-medium tw-c-background-alt tw-flex tw-full-width")[0];
-        // 檢查可否下注
-        var bettest0 = document.getElementsByClassName("fixed-prediction-button fixed-prediction-button--blue tw-align-items-center tw-border-radius-small tw-flex tw-pd-x-1 tw-pd-y-05")[0];
+        setTimeout(activityS, 300); // 0.3S 如果有第二層則打開
 
-        activityTabbet.click(); // First On
-        if (bettest !== undefined) {
-            bettest.click(); // Second On
-            if (bettest0 !== undefined) {
-                chrome.runtime.sendMessage({ content: true });
-                activityTabbet.click();
-            } else {
-                activityTabbet.click();
-            }
-        } else {
-            activityTabbet.click(); // 無偵測到第二層則關閉
-        }
+        setTimeout(activityT, 600); // 0.5S 如果有找到第三層發通知
+
+        setTimeout(activityF, 900); // 關閉全部
 
     } catch (error) {
         console.log(error);
     }
+}
+
+function activityF() {
+    var activityFirst = document.getElementsByClassName("ScCoreButton-sc-1qn4ixc-0 ScCoreButtonText-sc-1qn4ixc-3 kGWcAX tw-core-button")[0];
+    activityFirst.click();
+}
+
+function activityS() {
+    var activitySecond = document.getElementsByClassName("predictions-list-item__body tw-align-items-center tw-border-radius-medium tw-c-background-alt tw-flex tw-full-width")[0];
+    if (activitySecond !== undefined)
+        activitySecond.click();
+}
+
+function activityT() {
+    var detectBet = document.getElementsByClassName("fixed-prediction-button fixed-prediction-button--blue tw-align-items-center tw-border-radius-small tw-flex tw-pd-x-1 tw-pd-y-05")[0];
+    // var test = document.getElementsByClassName("tw-font-size-4 tw-line-height-heading tw-semibold tw-title tw-word-break-word")[0];
+    // console.log(test);
+    if (detectBet !== undefined)
+        chrome.runtime.sendMessage({ content: true });
 }
 
 detectBet();
