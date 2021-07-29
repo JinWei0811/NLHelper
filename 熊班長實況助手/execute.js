46d81a217ef6e97a9c62b2f65c127584c7a23bc7
JinWei0811 <31277660+JinWei0811@users.noreply.github.com>
2021-07-29 20:30:38 +0800

    v1.0.19
    調整 忠誠獎勵按鈕 class

--- 熊班長實況助手/execute.js
+++ 熊班長實況助手/execute.js
@@ -4,8 +4,8 @@ function goToActivityTab() {
         var nowm = today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
         var nowt = today.getSeconds() < 10 ? "0" + today.getSeconds() : today.getSeconds();
         var currentDateTime = today.getHours() + ':' + nowm + ":" + nowt;
-        var activityTab = document.getElementsByClassName("tw-button tw-button--success")[0];
-        console.log(currentDateTime + '  Point Detect');
+        var activityTab = document.getElementsByClassName("sc-fzozJi sc-fznKkj jwRWhW")[0];
+        // console.log(currentDateTime + '  Point Detect');
         if (activityTab != undefined) {
             activityTab.click();
             chrome.runtime.sendMessage({ activityTab: true });
