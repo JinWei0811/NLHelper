16125a885f7d118dcb5445084610b895f7221226
jingwei <jingwei@ximple.com.tw>
2022-02-09 14:26:20 +0800

    Update

--- 熊班長實況助手/execute.js
+++ 熊班長實況助手/execute.js
@@ -4,7 +4,7 @@ function goToActivityTab() {
         var nowm = today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
         var nowt = today.getSeconds() < 10 ? "0" + today.getSeconds() : today.getSeconds();
         var currentDateTime = today.getHours() + ':' + nowm + ":" + nowt;
-        var activityTab = document.getElementsByClassName("ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 fERWGf")[0];
+        var activityTab = document.getElementsByClassName("ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 boGviw iNPKuE")[0];
         // console.log(currentDateTime + '  Point Detect');
         if (activityTab != undefined) {
             activityTab.click();
